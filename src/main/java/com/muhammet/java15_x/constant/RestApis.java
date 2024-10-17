d45bdad362222a84d78250a9d33cf65f152755f9
bilgejava6 <bilgejava6@gmail.com>
2024-10-17 09:58:12 +0300

    Initial commit

---
+++ src/main/java/com/muhammet/java15_x/constant/RestApis.java
@@ -0,0 +1,13 @@
+package com.muhammet.java15_x.constant;
+
+public class RestApis {
+    private static final String VERSION = "/v1";
+    private static final String API = "/api";
+    private static final String DEVELOPER = "/dev";
+    private static final String TEST = "/test";
+    private static final String PROD = "/prod";
+
+    private static final String ROOT = VERSION+ DEVELOPER;
+
+
+}
